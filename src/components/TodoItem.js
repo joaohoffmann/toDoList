c6eaf491b2f3501d6ad0750d3406bbfb3b4ac1b1
João Pedro Hoffmann <joao.pedro.jordao09@gmail.com>
2022-05-12 14:22:25 -0300

    second commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+export const TodoItems = (props) => {
+  return (
+    <li>
+      <input type="checkbox" checked={props.item.isCompleted}
+      onChange={(event)=> props.onTaskChanged(event, props.item)}/>
+      {props.item.name}
+      </li>
+  )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import { TodoItems } from "./TodoItem"
+
+export const TodoList = (props) => {
+  const defaultListItems = [
+    {name: 'Tarefa 1', isCompleted: true},
+    {name: 'Tarefa 2', isCompleted: false},
+    {name: 'Tarefa 3', isCompleted: false},
+    {name: 'Tarefa 4', isCompleted: false}
+
+  ]
+  const [items, setItems] = useState(defaultListItems)
+  const [taskValue, setTaskvalue] = useState('')
+  const handleTaskValue = (event) => {
+    if(items.find(i => i.name === taskValue)){
+      event.preventDefault();
+      setTaskvalue('')
+      return (alert('Já existe uma tarefa com esse nome, por favor insira outro nome.'))
+
+    }
+    setItems([...items, {name: taskValue, isCompleted: false} ])
+    setTaskvalue('')
+    event.preventDefault();
+  }
+
+  const onTaskChanged = (event, item) =>{
+    setItems(items.map(i => {
+      if (i.name === item.name) {
+        return{
+          ...i,
+          isCompleted:event.target.checked
+        }
+      }
+      return i
+    }))
+  }
+  return(
+    <div className="to-do-list-container">
+      <p>{items.filter(i => i.isCompleted).length} done! Nice Job!</p>
+      <form onSubmit={handleTaskValue}>
+        <input placeholder="Qual a próxima tarefa?"
+        type="text" 
+        value={taskValue}
+        onChange={(e)=> setTaskvalue(e.target.value)}
+        />
+        <button type="submit">Adcionar Tarefa</button>
+      </form>
+
+     <ul>
+        {items.map(item => <TodoItems onTaskChanged={onTaskChanged} item={item}/> )}
+      </ul>
+    </div>
+  )
+}
